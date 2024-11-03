e7841880472d1e12f8431ee85670765f64c01d28
Arhan Ansari <arhanansari2009@gmail.com>
2024-11-03 22:02:42 +0530

    Update GitHubIcon.js

--- components/misc/GitHubIcon.js
+++ components/misc/GitHubIcon.js
@@ -3,7 +3,7 @@ import React from "react";
 const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/danielcranney/profileme-dev/"
+      href="https://github.com/ArhanAnsari/profileme-dev/"
       target="_blank"
       rel="noreferrer"
       className="no-underline"
