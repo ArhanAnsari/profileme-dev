05989519709ccd257f02f97d8476b1109e68ca9b
Arhan Ansari <arhanansari2009@gmail.com>
2024-11-03 21:49:11 +0530

    Update LandingLayout.js

--- components/layouts/LandingLayout.js
+++ components/layouts/LandingLayout.js
@@ -185,7 +185,7 @@ export default function LandingLayout({ children }) {
                   <li className="border-b border-light-100 dark:border-dark-600 h-10 flex group">
                     <Link
                       className="px-3 flex items-center font-normal no-underline w-full h-full text-dark-400/80 dark:text-light-300 hover:text-dark-700 dark:hover:text-white transition-all duration-150 ease-in-out gap-x-2 group"
-                      href="https://github.com/danielcranney/profileme-dev/"
+                      href="https://github.com/ArhanAnsari/profileme-dev/"
                       target="_blank"
                       rel="noreferrer"
                     >
@@ -207,25 +207,6 @@ export default function LandingLayout({ children }) {
                         GitHub Repo
                       </span>
                     </Link>
-                  </li>
-                  <li className="border-b-0 h-10 flex group">
-                    <a
-                      className="flex items-center px-3 font-normal no-underline w-full h-full text-dark-400/80 dark:text-light-300 dark:hover:text-white transition-all duration-150 ease-in-out hover:text-dark-700 gap-x-2 group  group-hover:text-dark-700 dark:group-hover:text-white"
-                      href="http://www.x.com/profilemedev"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <svg
-                        viewBox="0 0 128 128"
-                        className="w-5 h-4 group fill-current"
-                      >
-                        <path d="M40.254 127.637c48.305 0 74.719-48.957 74.719-91.403 0-1.39 0-2.777-.075-4.156 5.141-4.547 9.579-10.18 13.102-16.633-4.79 2.602-9.871 4.305-15.078 5.063 5.48-4.02 9.582-10.336 11.539-17.774-5.156 3.743-10.797 6.38-16.68 7.801-8.136-10.586-21.07-13.18-31.547-6.32-10.472 6.86-15.882 21.46-13.199 35.617C41.922 38.539 22.246 26.336 8.915 6.27 1.933 20.94 5.487 39.723 17.022 49.16c-4.148-.172-8.207-1.555-11.832-4.031v.41c0 15.273 8.786 28.438 21.02 31.492a21.596 21.596 0 01-11.863.543c3.437 13.094 13.297 22.07 24.535 22.328-9.305 8.918-20.793 13.75-32.617 13.72-2.094 0-4.188-.15-6.266-.446 12.008 9.433 25.98 14.441 40.254 14.422"></path>
-                      </svg>
-
-                      <span className="text-xs font-semibold uppercase">
-                        Twitter
-                      </span>
-                    </a>
                   </li>
                 </ul>
               </div>
@@ -315,7 +296,7 @@ export default function LandingLayout({ children }) {
                 Changelog
               </Link>
               <span className="text-dark-400 dark:text-white">·</span>
-              <a href="mailto:danielcranney@gmail.com" className="text-xs">
+              <a href="mailto:arhanansari2009@gmail.com" className="text-xs">
                 Leave Feedback
               </a>
             </div>
